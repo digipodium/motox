0c1ba18cd9b544032ad9fba28cce05bd32490f40
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-03 18:43:15 +0530

    templatesetup

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('',TemplateView.as_view(template_name='home/index.html')),
+    path('blog/',TemplateView.as_view(template_name='home/blog.html')),
+    path('about/',TemplateView.as_view(template_name='home/about.html')),
+    path('contact/',TemplateView.as_view(template_name='home/contact.html')),
+    path('services/',TemplateView.as_view(template_name='home/services.html')),
+    path('testimonial/',TemplateView.as_view(template_name='home/testimonial.html')),
+]
