cbc6c0906b85b2c8e2afee6c75001f8a55512df6
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-09 16:27:54 +0530

    form

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def index_view(request):
@@ -7,3 +8,22 @@ def index_view(request):
         form = request.POST
         print(form)
     return render(request, 'home/index.html',)
+
+def contact_view(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        subject = request.POST.get('subject')
+        # print(name, email, message, subject)
+        if name and email and message:
+            messages.success(request, 'Your message has been sent successfully.')
+        elif not name:
+            messages.error(request, 'Please enter your name.')
+        elif not email:
+            messages.error(request, 'Please enter your email.')
+        elif not message:
+            messages.error(request, 'Please enter your message.')
+        return redirect('contact')
+    return render(request, 'home/contact.html')
